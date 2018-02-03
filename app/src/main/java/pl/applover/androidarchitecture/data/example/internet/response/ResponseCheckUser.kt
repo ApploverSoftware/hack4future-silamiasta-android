1de40c1f02cf2c0b3d7889dbfc545eed9f8c5373
Janusz <januszhain@wp.pl>
2018-02-03 05:15:28 +0100

    Changed response body

--- app/src/main/java/pl/applover/androidarchitecture/data/example/internet/response/ResponseCheckUser.kt
+++ app/src/main/java/pl/applover/androidarchitecture/data/example/internet/response/ResponseCheckUser.kt
@@ -10,7 +10,9 @@ import io.mironov.smuggler.AutoParcelable
  */
 
 @SuppressLint("ParcelCreator")
-class ResponseCheckUser(@Json(name = "user") val user: User) : AutoParcelable {
+data class ResponseCheckUser(
+        @Json(name = "user") val user: ResponseCheckUser.User
+) : AutoParcelable {
 
     @SuppressLint("ParcelCreator")
     data class User(
@@ -18,16 +20,18 @@ class ResponseCheckUser(@Json(name = "user") val user: User) : AutoParcelable {
             @Json(name = "phone_number") val phoneNumber: String,
             @Json(name = "first_name") val firstName: String,
             @Json(name = "last_name") val lastName: String,
-            @Json(name = "balances") val balances: Balances
+            @Json(name = "balances") val balances: ResponseCheckUser.Balances
     ) : AutoParcelable
 
     @SuppressLint("ParcelCreator")
     data class Balances(
             @Json(name = "facebook") val facebook: Boolean,
             @Json(name = "instagram") val instagram: Boolean,
-            @Json(name = "email") val email: Boolean,
             @Json(name = "phone_number") val phoneNumber: Boolean,
+            @Json(name = "email") val email: Boolean,
+            @Json(name = "linked_in") val linkedIn: Boolean,
             @Json(name = "approval") val approval: Int,
             @Json(name = "disapproval") val disapproval: Int
     ) : AutoParcelable
+
 }
